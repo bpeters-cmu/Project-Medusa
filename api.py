4f36a3cc5dfee1ef2b3ce278205436c3f4199e5c
Ben Peters <bpeters@MacBook-Pro.local>
2017-10-05 12:36:28 -0500

    fixing imports

--- api.py
+++ api.py
@@ -3,17 +3,16 @@ from flask_restful import Resource
 from flask_httpauth import HTTPBasicAuth
 import traceback
 import app_service
+import models
 
 auth = HTTPBasicAuth()
 
 class Register(Resource):
-    def __init__(self):
-        from models import User
 
     def post(self):
         data = request.get_json(force=True)
         try:
-            new_user = User(data['username'], data['password'], data['tenancy_ocid'],
+            new_user = models.User(data['username'], data['password'], data['tenancy_ocid'],
             data['user_ocid'], data['fingerprint'], data['private_key'], data['region'])
             new_user.insert();
             return '200'
@@ -23,8 +22,6 @@ class Register(Resource):
 
 
 class Instance(Resource):
-    def __init__(self):
-        from models import User
 
     @auth.login_required
     def get(self, id=None):
@@ -39,14 +36,14 @@ class Instance(Resource):
 
     @auth.get_password
     def get_password(user):
-        user = User.query.filter_by(username=user).first()
+        user = models.User.query.filter_by(username=user).first()
         if user:
             return user.password
         return None
 
     @auth.verify_password
     def verify_password(username, password):
-        user = User.query.filter_by(username = username).first()
+        user = models.User.query.filter_by(username = username).first()
         if not user or not user.verify_password(password):
             return False
         g.user = user
